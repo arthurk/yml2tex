0be815e2f762f31d6dba0a50a38f397c178abe22
Arthur Koziel <arthur@arthurkoziel.com>
2009-01-03 18:36:39 +0100

    Added GPLv3 license information

--- yml2tex/__init__.py
+++ yml2tex/__init__.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 """
 Transform a YAML file into a LaTeX Beamer presentation.
 
-Usage: bin/yml2tex input.yml > output.tex
+Copyright (C) 2009 Arthur Koziel <arthur@arthurkoziel.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 __version__ = '1.2'
